860f032cf4adf6e3c0dd991ea6c54471cf243c90
Casko12 <phamduyhung.ftu@gmail.com>
2022-12-27 19:07:50 +0700

    C#

--- T2203E/Assignment2/PhoneBook.cs
+++ T2203E/Assignment2/PhoneBook.cs
@@ -3,10 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace T2203E_CSharp.Assignment2
 {
-    internal class PhoneBook : Phone
-    {
-    }
+    
+        public abstract class Phone
+        {
+           
+            public abstract void InsertPhone(String name, String phone);
+            public abstract void RemovePhone(String name);
+            public abstract void UpdatePhone(String name, String newPhone, String oldPhone);
+            public abstract PhoneNumber SearchPhone(String name);
+            public abstract void Sort();
+        }
+        public class PhoneBook: Phone
+        {
+            public PhoneBook ()
+            {
+                PhoneList = new List<PhoneNumber>();
+            }
+            public List<PhoneNumber> PhoneList { get; }
+            public override void InsertPhone(string name, string phone)
+            {
+                foreach(PhoneNumber p in PhoneList)
+                {
+                    if (p.Name.Equals(name))
+                    {
+                        if(p.Phone.Contains(phone))
+                        {
+                            return;
+                        }
+                        p.Phone.Add(phone);
+                        return;
+                    }
+                }
+                PhoneNumber pn = new PhoneNumber(name, phone);
+                PhoneList.Add(pn);
+            }
+            public override void RemovePhone(string name)
+            {
+                foreach(PhoneNumber p in PhoneList)
+                {
+                    if(p.Name.Equals(name))
+                    {
+                        PhoneList.Remove(p);
+                        return;
+                    }
+                }
+            }
+            public override void SearchPhone(string name)
+            {
+                foreach (PhoneNumber p in PhoneList)
+                {
+                    if (p.Name.Equals(name))
+                    {                        
+                        return p;
+                    }
+                }
+                return null;
+            }
+            public override void Sort()
+            {
+                PhoneList.Sort((o1, o2) =>
+                {
+                    return o1.Name.CompareTo(o2.Name);
+                });
+            }
+            public override void UpdatePhone()
+            {
+                foreach (PhoneNumber p in PhoneList)
+                {
+                    if (p.Name.Equals(name) && p.Phone.Contains(oldPhone))
+                    {
+                        p.Phone.Remove(oldPhone);
+                        p.Phone.Add(newPhone);
+                        return;
+                    }               
+                }
+            }
+
+        }
+    
 }
+
