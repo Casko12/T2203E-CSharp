0c1c34e7fbd3d98ca3b3fcaf267221d7879b7996
Casko12 <phamduyhung.ftu@gmail.com>
2023-01-10 18:23:47 +0700

    C#

---
+++ T2203E/PracticeExam/GeometryExample/Cylinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace T2203E_CSharp.PracticeExam.GeometryExample
+{
+    public class Cylinder
+    {
+        public Cylinder() { }
+        
+        public float BaseArea { get; set; }
+        public float Radius { get; set; }
+        public float LateralArea { get; set; }
+        public float Height { get; set; }
+        public float TotalArea { get; set; }
+        public float Volume { get; set; }
+
+        public void Process()
+        {
+            BaseArea = (float)(Radius*Radius*Math.PI);
+
+            LateralArea = (float)(2*Math.PI*Radius*Height);
+
+            TotalArea = (float)(2*Math.PI*Radius*(Height+Radius));
+
+            Volume = (float)(Math.PI*Radius*Radius*Height);
+
+        }
+
+        public void Result()
+        {
+
+        }
+
+
+    }
+}
